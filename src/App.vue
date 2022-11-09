8a882a71b958b4de3d0bee34eb457c9470e0f670
Eduardo Fernández <feguedi@yahoo.com.mx>
2022-11-08 19:40:20 -0600

    Primer commit

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<script setup>
+import { ref, unref } from 'vue';
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+
+const imagenArchivo = ref();
+const sonidoArchivo = ref();
+const videoSrc = ref('');
+const ffmpeg = createFFmpeg({
+  log: true,
+});
+
+function handleImagenArchivo(e) {
+  e.preventDefault();
+  console.log('handleImagenArchivo');
+  const file = e.target.files[0];
+  imagenArchivo.value = file;
+}
+
+function handleSonidoArchivo(e) {
+  e.preventDefault();
+  console.log('handleSonidoArchivo');
+  const file = e.target.files[0];
+  sonidoArchivo.value = file;
+}
+
+async function crearVideo() {
+  try {
+    console.log('crearVideo inicio');
+    await ffmpeg.load();
+    ffmpeg.FS('writeFile', 'image.png', await fetchFile(new Blob([unref(imagenArchivo)], { type: 'image/*' })));
+    ffmpeg.FS('writeFile', 'sound.mp3', await fetchFile(new Blob([unref(sonidoArchivo)], { type: 'sound/*' })));
+
+    await ffmpeg.run('-framerate', '1/10', '-i', 'image.png', '-i', 'sound.mp3', '-vcodec', 'libx264', '-t', '00:00:10', '-pix_fmt', 'yuv420p', '-vf', 'scale=1920:1080', 'test.mp4');
+    const data = await ffmpeg.FS('readFile', 'test.mp4');
+
+    videoSrc.value = URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' }));
+    console.log('crearVideo fin');
+  } catch (error) {
+    console.error('crearVideo error:', error.message || error);
+    throw new Error();
+  }
+}
+</script>
+
+<template>
+  <div class="mi-app">
+    <video :src="videoSrc" controls></video>
+
+    <input type="file" id="imagen" accept="image/*" :onChange="handleImagenArchivo">
+
+    <input type="file" id="sonido" accept="sound/*" :onChange="handleSonidoArchivo">
+
+    <button :onClick="crearVideo">Crear video</button>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+.mi-app {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    // https://github.com/vitejs/vite/issues/3909
+    {
+      name: 'configure-response-headers',
+      configureServer(server) {
+        server.middlewares.use((_req, res, next) => {
+          res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+          res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+          next();
+        });
+      },
+    },
+  ],
+});
